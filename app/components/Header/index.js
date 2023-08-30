4df776c01a2ffa9a3e909ebed0b17ec2f823b074
Walace Silva <wsilva.emp@gmail.com>
2023-08-30 00:00:49 -0300

    feat: added header component

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -3,26 +3,50 @@ import { FormattedMessage } from 'react-intl';
 
 import A from './A';
 import Img from './Img';
-import NavBar from './NavBar';
 import HeaderLink from './HeaderLink';
-import Banner from './banner.jpg';
 import messages from './messages';
+import LogoImage from '../../images/logo.png';
+import { Container, NavDropdown, Navbar } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart, faBars } from '@fortawesome/free-solid-svg-icons';
 
 function Header() {
   return (
-    <div>
-      <A href="https://www.reactboilerplate.com/">
-        <Img src={Banner} alt="react-boilerplate - Logo" />
-      </A>
-      <NavBar>
-        <HeaderLink to="/">
-          <FormattedMessage {...messages.home} />
-        </HeaderLink>
-        <HeaderLink to="/features">
-          <FormattedMessage {...messages.features} />
-        </HeaderLink>
-      </NavBar>
-    </div>
+    <header>
+      <Navbar className="navbar" expand="lg" style={{ backgroundColor: "#161616" }}>
+        <Container className="container container-common">
+          <div className="navbar-header">
+            <Navbar.Brand className="navbar-brand" href="/">
+              <img src={LogoImage} className="d-inline-block align-text-top" style={{ maxWidth: "150px" }} />
+            </Navbar.Brand>
+          </div>
+          <div className="d-flex flex-grow-1 flex-md-grow-0 justify-content-between justify-content-sm-end pl-2 pt-2 pt-md-0">
+            <div className="dropdown">
+              <a className="nav-link text-white">
+                <FontAwesomeIcon icon={faShoppingCart} size="lg" className="fa-icon" />
+                <span className="position-absolute start-80 translate-middle badge rounded-pill bg-danger">
+                  1
+                </span>
+              </a>
+            </div>
+            <div className="dropdown">
+              <a className="nav-link text-white dropdown-toggle" href="#" id="navbarDropdownMenuLinkIdiomas" role="button" data-bs-toggle="dropdown" aria-expanded="false" title="Alterar idioma">
+                <FontAwesomeIcon icon={faBars} size="lg" className="fa-icon" />
+              </a>
+              <div className="dropdown-menu dropdown-menu-end shadow" aria-labelledby="navbarDropdownMenuLinkIdiomas">
+                <a className="dropdown-item" href="">Inicio</a>
+                <a className="dropdown-item" href="">Sorteios</a>
+                <a className="dropdown-item" href="">Meus Números</a>
+                <a className="dropdown-item" href="">Cadastro</a>
+                <a className="dropdown-item" href="">Ganhadores</a>
+                <a className="dropdown-item" href="">Termos de uso</a>
+                <a className="dropdown-item" href="">Entre em contato</a>
+              </div>
+            </div>
+          </div>
+        </Container>
+      </Navbar>
+    </header>
   );
 }
 
