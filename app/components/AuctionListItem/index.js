6bd085905b16e501d95742d61fa1fe0ed98d77a4
Walace Silva <wsilva.emp@gmail.com>
2023-08-30 09:00:55 -0300

    fix: auction list item component

--- app/components/AuctionListItem/index.js
+++ app/components/AuctionListItem/index.js
@@ -15,20 +15,20 @@ import messages from './messages';
 function AuctionListItem() {
   return (
     <div className="card-product">
-      <div class="card mb-2">
-        <div class="row align-items-start g-0">
-          <div class="col-md-3 col-4 px-2 py-2">
+      <div className="card mb-2">
+        <div className="row align-items-start g-0">
+          <div className="col-md-3 col-4 px-2 py-2">
             <img src={ProductImage} className="img-fluid rounded" />
           </div>
-          <div class="col-md-9 col-8">
-            <div class="card-body p-2">
-              <h5 class="card-title">
+          <div className="col-md-9 col-8">
+            <div className="card-body p-2">
+              <h5 className="card-title">
                 Product title
               </h5>
-              <p class="card-text m-1">
+              <p className="card-text m-1">
                 This is a wider card
               </p>
-              <span class="badge bg-success font-xsss">Adquira já!</span>
+              <span className="badge bg-success font-xsss">Adquira já!</span>
             </div>
           </div>
         </div>
