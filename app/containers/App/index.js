60f13661b249a6b5eb0c0e087aacaa605001a090
Walace Silva <wsilva.emp@gmail.com>
2023-08-30 00:03:12 -0300

    refactor: app page

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -9,23 +9,26 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import styled from 'styled-components';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
 
 import HomePage from 'containers/HomePage/Loadable';
 import FeaturePage from 'containers/FeaturePage/Loadable';
+import AuctionListPage from 'containers/AuctionListPage/Loadable';
+import AuctionDetailPage from 'containers/AuctionDetailPage/Loadable';
+import CheckoutPage from 'containers/CheckoutPage/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
 import Header from 'components/Header';
 import Footer from 'components/Footer';
 
-import GlobalStyle from '../../global-styles';
+// import GlobalStyle from '../../global-styles';
 
 const AppWrapper = styled.div`
-  max-width: calc(768px + 16px * 2);
-  margin: 0 auto;
-  display: flex;
-  min-height: 100%;
-  padding: 0 16px;
-  flex-direction: column;
+  // max-width: calc(768px + 16px * 2);
+  // margin: 0 auto;
+  // display: flex;
+  // min-height: 100%;
+  // padding: 0 16px;
+  // flex-direction: column;
 `;
 
 export default function App() {
@@ -38,13 +41,17 @@ export default function App() {
         <meta name="description" content="A React.js Boilerplate application" />
       </Helmet>
       <Header />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/features" component={FeaturePage} />
-        <Route path="" component={NotFoundPage} />
-      </Switch>
-      <Footer />
-      <GlobalStyle />
+      <BrowserRouter>  
+        <Switch>
+          <Route exact path="/" component={AuctionListPage} />
+          <Route path="/features" component={FeaturePage} />
+          { /*<Route path="/sorteios" component={AuctionListPage} /> */ }
+          <Route path="/sorteio" component={AuctionDetailPage} />
+          <Route path="/checkout" component={CheckoutPage} />
+          <Route path="" component={NotFoundPage} />
+        </Switch>
+      </BrowserRouter>
+      { /*<GlobalStyle />*/ }
     </AppWrapper>
   );
 }
--- app/containers/AuctionDetailPage/index.js
+++ app/containers/AuctionDetailPage/index.js
@@ -14,14 +14,14 @@ import { compose } from 'redux';
 
 import { useInjectSaga } from 'utils/injectSaga';
 import { useInjectReducer } from 'utils/injectReducer';
+import { Form } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons';
 import makeSelectAuctionDetailPage from './selectors';
 import reducer from './reducer';
 import saga from './saga';
 import messages from './messages';
 import AuctionFeatured from '../../components/AuctionFeatured/Loadable';
-import { Form } from 'react-bootstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons';
 import CheckoutModal from '../CheckoutModal/Loadable';
 
 export function AuctionDetailPage() {
@@ -34,104 +34,122 @@ export function AuctionDetailPage() {
         <title>AuctionDetailPage</title>
         <meta name="description" content="Description of AuctionDetailPage" />
       </Helmet>
-      <div className="black-bar fuse"></div>
-        <div className="container container-common px-0">
-          <div className="main-content py-3">
-            <div>
-              <AuctionFeatured />
+      <div className="black-bar fuse" />
+      <div className="container container-common px-0">
+        <div className="main-content py-3">
+          <div>
+            <AuctionFeatured />
+          </div>
+          <div className="row">
+            <div className="col-12 text-center mb-3">
+              por apenas <span className="badge bg-dark">R$ 25,00</span>
             </div>
-            <div className="row">
-              <div className="col-12 text-center mb-3">
-                por apenas <span className="badge bg-dark">R$ 25,00</span>
-              </div>
-              <div className="col-6 mb-3">
-                Sorteio <span className="badge bg-light text-dark">21/08/2023 às 18h00</span>
-              </div>
-              <div className="col-6"></div>
-              <div className="col-12">
-                <div className="heading">
-                  <h1>⚡ Cotas</h1>
-                  <p className="desc">Escolha sua sorte</p>
-                </div>
+            <div className="col-6 mb-3">
+              Sorteio{' '}
+              <span className="badge bg-light text-dark">
+                21/08/2023 às 18h00
+              </span>
+            </div>
+            <div className="col-6" />
+            <div className="col-12">
+              <div className="heading">
+                <h1>⚡ Cotas</h1>
+                <p className="desc">Escolha sua sorte</p>
               </div>
             </div>
-            <div className="d-grid gap-2">
-              <button type="button" className="btn btn-success btn-sm">
-                <span>Ver meus números</span>
-              </button>
-            </div>
-            <Form action="/checkout" method="get">
-              <Form.Group className="mb-3" controlId="quantity">
-                <div className="card my-2">
-                  <div className="card-body">
-                    <div className="text-center mb-3 pt-2">
-                      <Form.Label className="text-muted font-xs">Selecione a quantidade de números</Form.Label>
-                    </div>
-                    <div className="d-flex align-items-center justify-content-center flex-column">
-                      <div className="sale-item">
-                        <div className="item p-2 me-2 mb-2 flex-column">
-                          <h3 className="mb-1">
-                            <small className="text-dark">+</small>05
-                          </h3>
-                          <p className="text-muted font-xss text-uppercase m-0">Selecionar</p>
-                        </div>
-                        <div className="item p-2 me-2 mb-2 flex-column popular">
-                          <h3 className="mb-1">
-                            <small className="text-dark">+</small>10
-                          </h3>
-                          <p className="text-muted font-xss text-uppercase">Selecionar</p>
-                        </div>
-                        <div className="item p-2 me-2 mb-2 flex-column">
-                          <h3 className="mb-1">
-                            <small className="text-dark">+</small>50
-                          </h3>
-                          <p className="text-muted font-xss text-uppercase">Selecionar</p>
-                        </div>
-                        <div className="item p-2 me-2 mb-2 flex-column">
-                          <h3 className="mb-1">
-                            <small className="text-dark">+</small>100
-                          </h3>
-                          <p className="text-muted font-xss text-uppercase">Selecionar</p>
-                        </div>
+          </div>
+          <div className="d-grid gap-2">
+            <button type="button" className="btn btn-success btn-sm">
+              <span>Ver meus números</span>
+            </button>
+          </div>
+          <Form action="/checkout" method="get">
+            <Form.Group className="mb-3" controlId="quantity">
+              <div className="card my-2">
+                <div className="card-body">
+                  <div className="text-center mb-3 pt-2">
+                    <Form.Label className="text-muted font-xs">
+                      Selecione a quantidade de números
+                    </Form.Label>
+                  </div>
+                  <div className="d-flex align-items-center justify-content-center flex-column">
+                    <div className="sale-item">
+                      <div className="item p-2 me-2 mb-2 flex-column">
+                        <h3 className="mb-1">
+                          <small className="text-dark">+</small>05
+                        </h3>
+                        <p className="text-muted font-xss text-uppercase m-0">
+                          Selecionar
+                        </p>
                       </div>
-                      <div className="text-center">
-                        <div className="vendasExpressNums d-flex align-items-center">
-                          <div className="left pointer">
-                            <div className="addNumero numeroChange text-muted">
-                              <FontAwesomeIcon icon={faMinus} size="xs" />
-                            </div>
+                      <div className="item p-2 me-2 mb-2 flex-column popular">
+                        <h3 className="mb-1">
+                          <small className="text-dark">+</small>10
+                        </h3>
+                        <p className="text-muted font-xss text-uppercase">
+                          Selecionar
+                        </p>
+                      </div>
+                      <div className="item p-2 me-2 mb-2 flex-column">
+                        <h3 className="mb-1">
+                          <small className="text-dark">+</small>50
+                        </h3>
+                        <p className="text-muted font-xss text-uppercase">
+                          Selecionar
+                        </p>
+                      </div>
+                      <div className="item p-2 me-2 mb-2 flex-column">
+                        <h3 className="mb-1">
+                          <small className="text-dark">+</small>100
+                        </h3>
+                        <p className="text-muted font-xss text-uppercase">
+                          Selecionar
+                        </p>
+                      </div>
+                    </div>
+                    <div className="text-center">
+                      <div className="vendasExpressNums d-flex align-items-center">
+                        <div className="left pointer">
+                          <div className="addNumero numeroChange text-muted">
+                            <FontAwesomeIcon icon={faMinus} size="xs" />
                           </div>
-                          <div className="center">
-                            <Form.Control type="text" aria-label="Quantidade de números" readOnly={true} placeholder="1" />
-                          </div>
-                          <div className="right pointer">
-                            <div className="removeNumero numeroChange text-muted">
-                              <FontAwesomeIcon icon={faPlus} size="xs" />
-                            </div>
+                        </div>
+                        <div className="center">
+                          <Form.Control
+                            type="text"
+                            aria-label="Quantidade de números"
+                            readOnly
+                            placeholder="1"
+                          />
+                        </div>
+                        <div className="right pointer">
+                          <div className="removeNumero numeroChange text-muted">
+                            <FontAwesomeIcon icon={faPlus} size="xs" />
                           </div>
                         </div>
                       </div>
                     </div>
                   </div>
                 </div>
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="submit">
-                <div className="d-grid gap-2">
-                  <button type="submit" className="btn btn-success">
-                    <div className="row align-items-center">
-                      <div className="col pe-0 text-nowrap">
-                        <span>Participar do sorteio</span>
-                      </div>
-                      <div className="col-auto ps-0">
-                        <span>R$ 100,00</span>
-                      </div>
+              </div>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="submit">
+              <div className="d-grid gap-2">
+                <button type="submit" className="btn btn-success">
+                  <div className="row align-items-center">
+                    <div className="col pe-0 text-nowrap">
+                      <span>Participar do sorteio</span>
                     </div>
-                  </button>
-                </div>
-              </Form.Group>
-            </Form>
-          </div>
+                    <div className="col-auto ps-0">
+                      <span>R$ 100,00</span>
+                    </div>
+                  </div>
+                </button>
+              </div>
+            </Form.Group>
+          </Form>
         </div>
+      </div>
       <CheckoutModal />
     </div>
   );
