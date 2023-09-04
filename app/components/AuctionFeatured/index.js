da72d4a8fb0eaeefa757524914de7a3370a5f224
Walace Silva <wsilva.emp@gmail.com>
2023-09-03 22:27:11 -0300

    feat: updated auction featured component

--- app/components/AuctionFeatured/index.js
+++ app/components/AuctionFeatured/index.js
@@ -8,47 +8,61 @@ import React, { memo } from 'react';
 // import PropTypes from 'prop-types';
 // import styled from 'styled-components';
 
-import ProductImage from './../../images/product-image.jpg';
 import { FormattedMessage } from 'react-intl';
+import ProductImage from '../../images/product-image.jpg';
 import messages from './messages';
 
 function AuctionFeatured({ product }) {
   return (
     <div>
       <div className="card mb-2">
-        <div id="carouselExampleFade" className="carousel slide carousel-fade" data-ride="carousel">
+        <div
+          id="carouselExampleFade"
+          className="carousel slide carousel-fade"
+          data-ride="carousel"
+        >
           <div className="carousel-inner">
             <div className="carousel-item active">
               <div className="card-feature-image">
-                <img src={product.images['url']} className="card-img-top" />
+                <img src={product.images.url} className="card-img-top" />
               </div>
             </div>
             <div className="carousel-item">
               <div className="card-feature-image">
-                <img src={product.images['url']} className="card-img-top" />
+                <img src={product.images.url} className="card-img-top" />
               </div>
             </div>
             <div className="carousel-item">
               <div className="card-feature-image">
-                <img src={product.images['url']} className="card-img-top" />
+                <img src={product.images.url} className="card-img-top" />
               </div>
             </div>
           </div>
-          <button className="carousel-control-prev" type="button" data-target="#carouselExampleFade" data-slide="prev">
-            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <button
+            className="carousel-control-prev"
+            type="button"
+            data-target="#carouselExampleFade"
+            data-slide="prev"
+          >
+            <span className="carousel-control-prev-icon" aria-hidden="true" />
             <span className="sr-only">Previous</span>
           </button>
-          <button className="carousel-control-next" type="button" data-target="#carouselExampleFade" data-slide="next">
-            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <button
+            className="carousel-control-next"
+            type="button"
+            data-target="#carouselExampleFade"
+            data-slide="next"
+          >
+            <span className="carousel-control-next-icon" aria-hidden="true" />
             <span className="sr-only">Next</span>
           </button>
         </div>
         <div className="card-body p-2">
-          <h5 className="card-title">
-            {product.title}
-          </h5>
+          <h5 className="card-title">{product.title}</h5>
           <p className="card-text">{product.description}</p>
-          <span className="badge bg-dark font-xsss">Corre que está acabando!</span>
+          <span className="badge bg-dark font-xsss">
+            Corre que está acabando!
+          </span>
         </div>
       </div>
     </div>
